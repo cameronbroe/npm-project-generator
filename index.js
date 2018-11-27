18ce4a89a72fcf18d0a59aaf82dbe18fdab55099
Cameron Roe <cameron.roe@smartbear.com>
2018-11-27 13:44:13 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const conv = require('number-to-words');
+
+let config;
+
+function loadConfig() {
+    config = JSON.parse(fs.readFileSync('./config.json'));
+}
+
+async function generateModule(number) {
+    let moduleLocals = {
+        num: number,
+        name: `is-${conv.toWords(number)}`.replace(/\s/g, '-'),
+        author: config.author
+    }
+    if(!fs.existsSync('build/')) {
+        fs.mkdirSync('build/');
+    }
+
+    if(!fs.existsSync(`build/${moduleLocals.name}/`)) {
+        fs.mkdirSync(`build/${moduleLocals.name}/`);
+    }
+
+    let indexJs = await parseTemplate('template/index.js.tmpl', moduleLocals);
+    let packageJson = await parseTemplate('template/package.json.tmpl', moduleLocals);
+    let writeCallback = (err) => {
+        if(err) throw err;
+        // console.log(`${moduleLocals.name} file written`);
+    }
+    fs.writeFile(`build/${moduleLocals.name}/index.js`, indexJs, writeCallback);
+    fs.writeFile(`build/${moduleLocals.name}/package.json`, packageJson, writeCallback);
+}
+
+async function parseTemplate(filename, locals) {
+    let templateStr = fs.readFileSync(filename, { encoding: 'utf-8' });
+    Object.keys(locals).forEach((key) => {
+        templateStr = templateStr.replace(`|=${key}=|`, locals[key]);
+    });
+    return templateStr;
+}
+
+function main() {
+    loadConfig();
+
+    for(i = 0; i < 1000; i++) {
+        generateModule(i);
+    }
+}
+
+if(require.main === module) {
+    main()
+}
